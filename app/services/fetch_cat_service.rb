73f32995921ba7e35816ee12ebc8420ce970636d
rupertlion <rupertlion@gmail.com>
2018-10-03 18:11:18 +0200

    modifying api call and index show

--- app/services/fetch_cat_service.rb
+++ app/services/fetch_cat_service.rb
@@ -5,7 +5,7 @@ module FetchCatService
         headers = {'Content-Type': 'application/json', 'x-api-key': 'a00af988-a101-49ae-b4a2-3be11aed392c'}
         url = "https://api.thecatapi.com/v1/images/search?format=json&limit=2"
         @cats_fetched = JSON.parse(RestClient.get(url, headers).body)
-        # @cat1 = cat_fetched.first
-        # @cat2 = cat_fetched.second
+        @cat1 = @cats_fetched.first['url']
+        @cat2 = @cats_fetched.second['url']
     end
 end
