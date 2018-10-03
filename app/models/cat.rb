3a1e99d6fe1e7647d676312f38e9261d0634417b
rupertlion <rupertlion@gmail.com>
2018-10-03 16:45:27 +0200

    Added index function in CatsController, add validation to Cat model, updated application.html with haml

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,3 @@
 class Cat < ApplicationRecord
+    validates :image, presence: true
 end
--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,3 @@
 Then("I should see {string} images") do |number|
-    expect(page).to have_css, ('.imageclass', count: number)
+    expect(page).to have_css, count: number
 end
