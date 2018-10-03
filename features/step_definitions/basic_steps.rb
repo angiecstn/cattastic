b95cae0f2226f88594ce12e42c4f436c93d9ac2b
Zak <zakary.howard94@gmail.com>
2018-10-03 19:48:04 +0200

    moved then steps to assertion steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,11 +1,3 @@
-When("I visit the site") do
-    visit root_path
-end
-  
-Then("I should see {string}") do |content|
-    expect(page).to have_content content
-end
-  
 Given("the following categories exists") do |table|
     table.hashes.each do |category|
       Category.create!(category)
