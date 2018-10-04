c8089054172db0c851cbc3c2f44921604c9554fb
Zak <zakary.howard94@gmail.com>
2018-10-04 14:50:11 +0200

    refactoring

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -6,11 +6,11 @@ Then("I should see {string}") do |content|
     expect(page).to have_content content
 end
 
-When("I click {string} button") do |link_name|
-    click_on link_name
-end
-
 Then("I should be on {string} page") do |category_title|
     category = Category.find_by_title(category_title)
     expect(current_path).to eq category_path(category)
+end
+
+When("I click {string} button") do |link_name|
+    click_on link_name
 end
