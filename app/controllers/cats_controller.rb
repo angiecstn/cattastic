0eaec8d6e266606db88298f2b9bacbf61517a35f
Angelica Costantini <angelicacostantini07@gmail.com>
2018-10-04 18:28:26 +0200

    Creating new cats saved in DB, need to refactor to iterate

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,11 @@ class CatsController < ApplicationController
   def index
     @cats = FetchCatService.get_cat_image
   end
+
+  def create
+    @cats = FetchCatService.get_cat_image
+    cat1 = Cat.create(url: @cats.first['url'])
+    cat2 = Cat.create(url: @cats.last['url'])
+    binding.pry
+  end
 end
