83cee8924685f4904ec8725477404aa552b99684
rupertlion <rupertlion@gmail.com>
2018-10-04 10:11:17 +0200

    Updated credentials for API and called in service

--- app/services/fetch_cat_service.rb
+++ app/services/fetch_cat_service.rb
@@ -2,7 +2,7 @@ module FetchCatService
     require 'rest-client'
 
     def self.get_cat_image
-        headers = {'Content-Type': 'application/json', 'x-api-key': 'a00af988-a101-49ae-b4a2-3be11aed392c'}
+        headers = {'Content-Type': 'application/json', 'x-api-key': Rails.application.credentials.catapi[:api_key] }
         url = "https://api.thecatapi.com/v1/images/search?format=json&limit=2"
         JSON.parse(RestClient.get(url, headers).body)
     end
