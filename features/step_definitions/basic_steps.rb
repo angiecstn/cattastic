5336c471fe5dbf9f289ff14c9a8c05e9f2a14204
rupertlion <rupertlion@gmail.com>
2018-10-04 12:26:46 +0200

    Moving table.hashes to new_object_steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,12 +2,6 @@ Given("I am on the landing page") do
     visit root_path
 end
 
-Given("the following categories exist") do |table|
-    table.hashes.each do |category|
-      create(:category, category)
-    end
-end
-
 Then ("show me the page") do
     save_and_open_page
 end
