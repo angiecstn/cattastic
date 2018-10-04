f42c0d1a4e33d79d6175f72168e43d4cb2580940
rupertlion <rupertlion@gmail.com>
2018-10-04 09:24:53 +0200

    Deleted migration and DB schema

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,7 +1,3 @@
-# Then('I should see {string} images') do |string|
-#     expect(page).to have_css count: string
-# end
-
 Then("I should see {string} images") do |number| 
     expect(page).to have_css '.cat', count: number
 end
