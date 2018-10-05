81ac281174354df0b36de6dacef1cb908d6d7721
Zak <zakary.howard94@gmail.com>
2018-10-05 13:22:27 +0200

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root controller: :cats, action: :index
   resources :categories, only: [:index, :show]
+  root 'cats#index'
 end
+
+# root 'static_pages#index'
