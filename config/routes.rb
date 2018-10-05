53448d9918ac8f4215c66294fb766104d0eca09e
Zak <zakary.howard94@gmail.com>
2018-10-05 14:20:15 +0200

    created navbar with link to sign up, removed old syntax root path, migrated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root controller: :cats, action: :index
   resources :categories, only: [:index, :show]
-  root 'cats#index'
 end
-
-# root 'static_pages#index'
