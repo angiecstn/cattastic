2cec917540c56451d110706844d229c23814e50e
Zak <zakary.howard94@gmail.com>
2018-10-05 16:11:34 +0200

    added validations, added factory bot to spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_presence_of :password }
   end
+  
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:user)).to be_valid 
+    end
+  end
 end
